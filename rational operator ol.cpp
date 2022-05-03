00af46be3d136f42e9c423e26fa1c78d0254317e
Nischal Neupane <Leonischu10@gmail.com>
2022-05-03 20:58:11 +0545

    error output

---
+++ rational operator ol.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class rational
+{
+    int num,den;
+    public:
+    rational()
+    {
+        num=0;
+        den=0;
+    }
+    rational(int n,int d){
+        num=n;
+        den=d;
+        
+    }
+    void displayrational(){
+        cout<<"Rational number is:"<<num<<"/"<<den<<endl;
+    }
+    rational operator+(rational r2){
+        rational r;
+        r.num=num+r2.den+num*r2.den;
+        r.den=den*r2.den;
+        return r;
+    }
+};
+int main(){
+     rational r1(2,3),r2(3,4),r3;
+     r1.displayrational();
+     r2.displayrational();
+     r3=r1+r2;
+     cout<<"After addition:"<<endl;
+     r3.displayrational();
+     getch();
+}
