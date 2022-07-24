c3d56c5ac9458b48d08317be042ea0a82dcf30fa
Nischal Neupane <Leonischu10@gmail.com>
2022-07-24 12:20:02 +0545

    corrected

--- day1.cpp
+++ day1.cpp
@@ -1,5 +1,3 @@
-//program to find the difference betwen two complex number 
-//by overloading + and - operator.
 #include<iostream>
 #include<conio.h>
 using namespace std;
@@ -11,16 +9,16 @@ class complex{
     complex(){
         real=0.0;
         img=0.0;
-        
+
     }
     complex(float x, float y){
         real= x;
         img =y;
-        
-        
+
+
     }
-    void outdata(char *msg){
-        cout<<endl<<msg;
+    void  outdata(){
+        
         cout<<img<<real<<endl;
     }
     friend complex operator+(complex,complex);
@@ -29,18 +27,26 @@ class complex{
 complex operator+(complex c1,complex c2){
     complex temp;
     temp.real=c1.real+c2.real;
+    temp .img=c1.img+c2.img;
+    return temp;
+
+}
+complex operator-(complex c1,complex c2){
+    complex temp;
+    temp.real=c1.real-c2.real;
     temp .img=c1.img-c2.img;
     return temp;
-    
+
 }
 int main(){
     complex c1(2.5,3.5),c2(1.6,2.7),c3;
     c1.outdata();
     c2.outdata();
     c3=c1+c2;
-    c3.outdata;
+    c3.outdata();
     c3=c1-c2;
     c3.outdata();
     return 0;
 }
-}
+
+ss
